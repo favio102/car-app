7523a5187fc177d3ddcb1e83f47fce4e38533e27
Favio V. F. <favio102@protonmail.com>
2024-06-21 11:09:55 -0700

    hero image added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,7 +21,12 @@ const Hero = () => {
           handleClick={handleScroll}
         />
       </div>
-      <div className="hero__image-container"></div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay"/>
+        </div>
+      </div>
     </div>
   );
 };
