220790d51d94ed6bd085b83c32cbde7255f9aef7
Favio V. F. <favio102@protonmail.com>
2024-06-29 20:57:57 -0700

    search bar component update

--- utils/index.ts
+++ utils/index.ts
@@ -29,14 +29,14 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 };
 
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
-  const url = new URL("https://cdn.imagin.studio/getimage");
+  const url = new URL(`https://cdn.imagin.studio/getimage`);
   const { make, year, model } = car;
 
   url.searchParams.append("customer", `${process.env.KEY_CAR_IMAGE_API}`);
   url.searchParams.append("make", make);
-  url.searchParams.append("modelFamily", make.split(" ")[0]);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
   url.searchParams.append("zoomType", "fullscreen");
   url.searchParams.append("modelYear", `${year}`);
   url.searchParams.append("angle", `${angle}`);
-  return `${url}`;
+  return url.toString();
 };
