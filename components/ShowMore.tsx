38c3e620d17b2ca76c9930f0852cd67336be0317
Favio V. F. <favio102@protonmail.com>
2024-06-30 05:59:21 -0700

    fix showmore component

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,5 +1,5 @@
 import { ShowMoreProps } from "@/types";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React from "react";
 import CustomButton from "./CustomButton";
 import { updateSearchParams } from "@/utils";
@@ -10,7 +10,7 @@ const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const handleNavigation = () => {
     const newLimit = (pageNumber + 1) * 10;
     const newPathName = updateSearchParams("limit", `${newLimit}`);
-    router.push(newPathName, undefined, { scroll: false });
+    router.push(newPathName, { scroll: false });
   };
 
   return (
